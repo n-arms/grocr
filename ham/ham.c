dd73c8247adb6b2af175ad2385683831227daf75
Olive Hudson <whudson@uwaterloo.ca>
2024-11-21 16:56:56 -0500

    complete hamming error correction codes code

---
+++ ham/ham.c
@@ -0,0 +1,82 @@
+/* ham - hamming error correction codes
+ * Copyright (C) 2024 Olive Hudson <whudson@uwaterloo.ca>
+ * see LICENCE file for licensing information */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "ham.h"
+
+static inline bool pow2(int val);
+
+/* returns true if a number is a power of two or zero */
+static inline bool
+pow2(int val)
+{
+	return (val & (val - 1)) == 0;
+}
+
+uint16_t
+enc(uint16_t val) /* val >> 11 == 0 */
+{
+	uint16_t code = 0;
+
+	/* create spaces at 0, 1, 2, 4, 8th bits */
+	for (int8_t i = 15, ind = 10; i > 0; --i) {
+		if (!pow2(i)) {
+			code |= ((val >> ind) & 1);
+			--ind;
+		}
+		code <<= 1;
+	}
+
+	for (uint8_t i = 1; i <= 8; i *= 2) {
+		uint8_t cnt = 0;
+		for (uint8_t j = i + 1; j < 16; ++j)
+			if (i & j)
+				cnt += ((code >> j) & 1);
+		code |= ((cnt & 1) << i);
+	}
+
+	uint8_t cnt = 0;
+	for (uint8_t i = 0; i < 16; ++i)
+		cnt += ((code >> i) & 1);
+	code |= (cnt & 1);
+
+	return code;
+}
+
+uint16_t
+dec(uint16_t code)
+{
+	uint8_t shift = 0;
+	for (uint8_t i = 1; i <= 8; i *= 2) {
+		uint8_t cnt = 0;
+		for (uint8_t j = i + 1; j < 16; ++j)
+			if (i & j)
+				cnt += ((code >> j) & 1);
+
+		shift |= ((cnt & 1) != ((code >> i) & 1)) << 4;
+		shift >>= 1;
+	}
+
+	if (shift != 0) {
+		uint8_t cnt = 0;
+		for (uint8_t i = 1; i < 16; ++i)
+			cnt += ((code >> i) & 1);
+		if ((cnt & 1) == (code & 1))
+			return 0; /* two bit error */
+
+		code ^= 1 << shift;
+	}
+
+	uint16_t val = 0;
+	for (int8_t i = 15; i > 2; --i) {
+		if (!pow2(i)) {
+			val <<= 1;
+			val |= (code >> i) & 1;
+		}
+	}
+
+	return val;
+}
---
+++ ham/ham.h
@@ -0,0 +1,36 @@
+/* ham - hamming error correction codes
+ * Copyright (C) 2024 Olive Hudson <whudson@uwaterloo.ca>
+ * see LICENCE file for licensing information */
+
+#ifndef HAM_H
+#define HAM_H
+
+/* hamming encoding uses a series of parity bits at specific positions in the
+ * data to check for single bit flips. an additional parity bit can be used to
+ * check for double bit flips.
+ *
+ * this specific implementation encodes eleven bits with four hamming parity
+ * bits and one extra parity bit to have an encoded size of sixteen bits.
+ *
+ * to encode, creates spaces in the value at bit positions 0, 1, 2, 4, and 8.
+ * for bit positions 1, 2, 4, and 8, they are defined as the parity of the
+ * specific bits of the bits in the spaced value which have the specific power
+ * of two in their bit pattern. for bit position 0, that is the parity of the
+ * entire code after the other parity bits have been populated.
+ *
+ * to decode, the parity bits at the bit positions 1, 2, 4, and 8 are verified.
+ * a zero is added to the specific power of two bit position in a new varible
+ * if the parity checks match, and a one is otherwise. this variable is the
+ * position of the bit which was flipped if it is non-zero.
+ *
+ * if the variable is non-zero and the parity bit at bit position zero is
+ * accurate, a single bit flip is assumed, otherwise a double bit flip is
+ * assumed and zero is returned instead of a corrected value. */
+
+/* hamming encodes an eleven bit value */
+uint16_t enc(uint16_t val);
+
+/* hamming decodes an eleven bit value */
+uint16_t dec(uint16_t val);
+
+#endif /* HAM_H */
