7e2367f0205c27980837f7994e8d722e78e50cb7
n-arms <75282394+n-arms@users.noreply.github.com>
2024-11-21 18:10:18 -0500

    Implement driver modulo hardware config

--- lc/lc.h
+++ lc/lc.h
@@ -9,14 +9,23 @@ typedef struct hx711_config {
   uint32_t dout_pin;
   GPIO_TypeDef *pd_sck_gpio;
   uint32_t pd_sck_pin;
+  TIM_HandleTypeDef *timer;
 };
 
 typedef struct hx711_driver {
   hx711_config config;
+  bool * volatile data;
+  volatile uint32_t current_index;
+  volatile bool clock_high;
+  volatile bool has_data;
 } hx711_driver;
 
-hx711_driver new_hx711_driver(hx711_config config);
-void tick_hx711_driver(hx711_driver *driver);
+// should be the only instance of hx711_driver,
+// as the interrupt magic relies on talking to this global singleton
+extern hx711_driver hx711;
+extern bool hx711_data[27];
+
+void init_hx711_driver(hx711_config config);
 bool poll_hx711_driver(hx711_driver *driver);
 uint32_t get_hx711_driver(hx711_driver *driver);
 
