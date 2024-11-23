ea9e1a663099163e9ecee8219fbe6f6be9361a0c
n-arms <75282394+n-arms@users.noreply.github.com>
2024-11-23 18:49:13 -0500

    Add documentation for I2C.

--- i2c/inc/i2c_config.h
+++ i2c/inc/i2c_config.h
@@ -1,6 +1,7 @@
 #ifndef __I2C_CONFIG_H
 #define __I2C_CONFIG_H
 
+/* Configuration for an I2C transmitter driver. */
 typedef struct I2C_tx_config
 {
     // GPIO config
@@ -13,6 +14,7 @@ typedef struct I2C_tx_config
     uint64_t millis_per_tick;
 } I2C_tx_config;
 
+/* Configuration for an I2C receiver driver. */
 typedef struct I2C_rx_config
 {
     // GPIO config
--- i2c/inc/i2c_rx.h
+++ i2c/inc/i2c_rx.h
@@ -7,6 +7,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/* This module implements the driver for receiving I2C packets.
+
+Example usage:
+
+I2C_rx_config config;
+I2C_rx_driver d = new_I2C_rx_driver(config, backing_array, 24, 500)
+
+while (1) {
+	tick_I2C_rx_driver(&d);
+	if (poll_I2C_driver(&d)) {
+		bool *data = get_I2C_driver(&d);
+
+		// use data
+
+		reset_I2C_driver(&d);
+	}
+}
+*/
+
+/* The state and configuration of the driver. */
 typedef struct I2C_rx_driver
 {
 	uint64_t last_tick;
@@ -18,11 +38,17 @@ typedef struct I2C_rx_driver
 	I2C_rx_config config;
 } I2C_rx_driver;
 
+/* Create a new I2C driver. */
 I2C_rx_driver new_I2C_rx_driver(I2C_rx_config config, bool *data, size_t packet_size, uint64_t message_dead_time);
+
+/* Update the state of the I2C driver.
+Should be called in a loop at least as frequently as the tick rate of the attached I2C_tx driver. */
 void tick_I2C_rx_driver(I2C_rx_driver *driver);
+/* Check if the driver has new data. */
 bool poll_I2C_driver(I2C_rx_driver *driver);
-// only valid data if `poll_I2C_driver` returns true
+/* Get the most recent packet received. Only valid if `poll_I2C_driver` returns true. */
 bool *get_I2C_driver(I2C_rx_driver *driver);
+/* Mark the most recently received packet as "stale", so that the driver is ready to a receive new packets. */
 void reset_I2C_driver(I2C_rx_driver *driver);
 
 #endif
