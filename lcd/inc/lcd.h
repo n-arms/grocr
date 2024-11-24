463e6c7b13994c6f2e49fc18f8afef191d9efd08
Jason <jason@jasonx.dev>
2024-11-23 20:45:54 -0500

    rename functions to prefix with lcd_

--- lcd/inc/lcd.h
+++ lcd/inc/lcd.h
@@ -5,17 +5,22 @@
 #ifndef LCD_H
 #define LCD_H
 
-typedef enum enable {
-        DISABLE = 0,
-        ENABLE = 1
+#include "stm32f4xx_hal.h"
+
+typedef enum enable
+{
+        LCD_DISABLE = 0,
+        LCD_ENABLE = 1
 } enable_t;
 
-typedef enum rw {
+typedef enum rw
+{
         WRITE = 0,
         READ = 1
 } rw_t;
 
-typedef enum select {
+typedef enum select
+{
         COMMAND = 0,
         DATA = 1
 } select_t;
@@ -26,16 +31,17 @@ typedef enum select {
  * - select -> RS
  * - data -> DB7 DB6 DB5 DB4 (in this order)
  * - enable -> E */
-typedef struct lcd {
-	GPIO_TypeDef *lcd_misc_gpio;
-	uint32_t select_pin;
-	uint32_t rw_pin;
-	uint32_t enable_pin;
-	GPIO_TypeDef *lcd_data_gpio;
-	uint32_t data_pin_1;
-	uint32_t data_pin_2;
-	uint32_t data_pin_3;
-	uint32_t data_pin_4;
+typedef struct lcd
+{
+        GPIO_TypeDef *lcd_misc_gpio;
+        uint32_t select_pin;
+        uint32_t rw_pin;
+        uint32_t enable_pin;
+        GPIO_TypeDef *lcd_data_gpio;
+        uint32_t data_pin_1;
+        uint32_t data_pin_2;
+        uint32_t data_pin_3;
+        uint32_t data_pin_4;
         volatile rw_t rw : 1;
         volatile select_t select : 1;
         volatile uint8_t data : 4;
@@ -43,8 +49,8 @@ typedef struct lcd {
 } lcd_t;
 
 /* resets the lcd after powering on */
-void reset(lcd_t *lcd);
+void lcd_reset(lcd_t *lcd);
 /* resets lines of text on lcd */
-void string(lcd_t *lcd, const char *line1, const char *line2);
+void lcd_string(lcd_t *lcd, const char *line1, const char *line2);
 
 #endif /* LCD_H */
--- lcd/src/lcd.c
+++ lcd/src/lcd.c
@@ -7,40 +7,40 @@
 
 #include "lcd.h"
 
-static void enable(lcd_t *lcd);
-static void wait(lcd_t *lcd);
-static void send(lcd_t *lcd, uint8_t cmd, select_t select);
+static void lcd_enable(lcd_t *lcd);
+static void lcd_wait(lcd_t *lcd);
+static void lcd_send(lcd_t *lcd, uint8_t cmd, select_t select);
 
 static void
-write(lcd_t *lcd)
+lcd_write(lcd_t *lcd)
 {
-	//WRITE(PIN0, lcd->select);
+	// WRITE(PIN0, lcd->select);
 	HAL_GPIO_WritePin(lcd->lcd_misc_gpio, lcd->select_pin, lcd->select);
-	//WRITE(PIN1, lcd->rw);
+	// WRITE(PIN1, lcd->rw);
 	HAL_GPIO_WritePin(lcd->lcd_misc_gpio, lcd->rw_pin, lcd->rw);
-	//WRITE(PIN2, lcd->enable);
+	// WRITE(PIN2, lcd->enable);
 	HAL_GPIO_WritePin(lcd->lcd_misc_gpio, lcd->enable_pin, lcd->enable);
 
-	//WRITE(PIN4, lcd->data & 0x1);
+	// WRITE(PIN4, lcd->data & 0x1);
 	HAL_GPIO_WritePin(lcd->lcd_data_gpio, lcd->data_pin_1, lcd->data & 0x1);
-	//WRITE(PIN5, (lcd->data & 0x2) >> 1);
+	// WRITE(PIN5, (lcd->data & 0x2) >> 1);
 	HAL_GPIO_WritePin(lcd->lcd_data_gpio, lcd->data_pin_2, (lcd->data & 0x2) >> 1);
-	//WRITE(PIN6, (lcd->data & 0x4) >> 2);
+	// WRITE(PIN6, (lcd->data & 0x4) >> 2);
 	HAL_GPIO_WritePin(lcd->lcd_data_gpio, lcd->data_pin_3, (lcd->data & 0x4) >> 2);
-	//WRITE(PIN7, (lcd->data & 0x8) >> 3);
+	// WRITE(PIN7, (lcd->data & 0x8) >> 3);
 	HAL_GPIO_WritePin(lcd->lcd_data_gpio, lcd->data_pin_4, (lcd->data & 0x8) >> 3);
 }
 
 /* commands and data are indicated to be sent on a low pulse on the enable
  * pin */
 static void
-enable(lcd_t *lcd)
+lcd_enable(lcd_t *lcd)
 {
-	lcd->enable = ENABLE;
-	write(lcd);
+	lcd->enable = LCD_ENABLE;
+	lcd_write(lcd);
 	/* delay 450ns */
-	lcd->enable = DISABLE;
-	write(lcd);
+	lcd->enable = LCD_DISABLE;
+	lcd_write(lcd);
 }
 
 /* sending commands and data may not be effective if the internal processor is
@@ -48,7 +48,7 @@ enable(lcd_t *lcd)
  * necessary to check the busy flag and only send more data once it clears.
  * doing so has the added benefit of making commands finish quicker. */
 static void
-wait(lcd_t *lcd)
+lcd_wait(lcd_t *lcd)
 {
 	lcd_t copy;
 	memcpy(&copy, lcd, sizeof(lcd_t));
@@ -56,10 +56,10 @@ wait(lcd_t *lcd)
 	lcd->rw = READ;
 	lcd->select = COMMAND;
 	lcd->data = 0x8;
-	write(lcd);
+	lcd_write(lcd);
 
 	do
-		enable(lcd);
+		lcd_enable(lcd);
 	while ((lcd->data & 0x8) == 0x8);
 
 	memcpy(lcd, &copy, sizeof(lcd_t));
@@ -68,54 +68,52 @@ wait(lcd_t *lcd)
 /* this function implements the algorithm necessary to send a command or
  * write data using only four data pins rather than the typical eight. */
 static void
-send(lcd_t *lcd, uint8_t cmd, select_t select)
+lcd_send(lcd_t *lcd, uint8_t cmd, select_t select)
 {
 	lcd->rw = WRITE;
 	lcd->select = select;
 	lcd->data = (cmd & 0xF0) >> 4;
-	write(lcd);
-	enable(lcd);
+	lcd_write(lcd);
+	lcd_enable(lcd);
 
-	wait(lcd);
+	lcd_wait(lcd);
 
 	lcd->data = (cmd & 0xF);
-	write(lcd);
-	enable(lcd);
+	lcd_write(lcd);
+	lcd_enable(lcd);
 
-	wait(lcd);
+	lcd_wait(lcd);
 }
 
 /* a reset sequence must be run each time the lcd is powered on to enter four
  * bit operation. */
-void
-reset(lcd_t *lcd)
+void lcd_reset(lcd_t *lcd)
 {
 	lcd->rw = WRITE;
 	lcd->select = COMMAND;
 	lcd->data = 0x3;
-	write(lcd);
+	lcd_write(lcd);
 
 	/* delay 40ms */
-	enable(lcd);
+	lcd_enable(lcd);
 	/* delay 4.1ms */
-	enable(lcd);
+	lcd_enable(lcd);
 	/* delay 100us */
 
-	send(lcd, COMMAND, 0x2C); /* four bit input, two lines, 5x10 font */
-	send(lcd, COMMAND, 0x8);  /* display off */
-	send(lcd, COMMAND, 0x1);  /* clear and return to first line */
-	send(lcd, COMMAND, 0x7);  /* shift right after char input */
-	send(lcd, COMMAND, 0xC);  /* display on, cursor off */
+	lcd_send(lcd, COMMAND, 0x2C); /* four bit input, two lines, 5x10 font */
+	lcd_send(lcd, COMMAND, 0x8);  /* display off */
+	lcd_send(lcd, COMMAND, 0x1);  /* clear and return to first line */
+	lcd_send(lcd, COMMAND, 0x7);  /* shift right after char input */
+	lcd_send(lcd, COMMAND, 0xC);  /* display on, cursor off */
 }
 
-void
-string(lcd_t *lcd, const char *line1, const char *line2)
+void lcd_string(lcd_t *lcd, const char *line1, const char *line2)
 {
-	send(lcd, COMMAND, 0x1); /* clear and return to first line */
+	lcd_send(lcd, COMMAND, 0x1); /* clear and return to first line */
 	/* both strings limited to sixteen characters */
-	for (uint8_t i = 0; i < 16 && str1[i] != '\0'; ++i)
-		send(lcd, DATA, str1[i]);
-	send(lcd, COMMAND, 0xC0); /* jump to second line */
-	for (uint8_t i = 0; i < 16 && str2[i] != '\0'; ++i)
-		send(lcd, DATA, str2[i]);
+	for (uint8_t i = 0; i < 16 && line1[i] != '\0'; ++i)
+		lcd_send(lcd, DATA, line1[i]);
+	lcd_send(lcd, COMMAND, 0xC0); /* jump to second line */
+	for (uint8_t i = 0; i < 16 && line2[i] != '\0'; ++i)
+		lcd_send(lcd, DATA, line2[i]);
 }
