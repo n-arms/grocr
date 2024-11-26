10452ab255adbfa3a9976b190f1818bbd42137bc
Olive Hudson <whudson@uwaterloo.ca>
2024-11-25 22:43:25 -0500

    add documentation to lcd functions

--- lcd/inc/lcd.h
+++ lcd/inc/lcd.h
@@ -7,12 +7,15 @@
 
 #include "stm32f4xx_hal.h"
 
+/* lcd_t must be populated by gpio configurations and pin numbers */
 typedef struct lcd {
 	GPIO_TypeDef *gpio_etc, *gpio_data;
 	uint32_t pin_select, pin_enable, pin1, pin2, pin3, pin4;
 } lcd_t;
 
+/* lcd_reset must be run before information can be written to the lcd */
 void lcd_reset(lcd_t *lcd);
+/* lcd_str writes to the two lines of the lcd display */
 void lcd_str(lcd_t *lcd, const char *line1, const char *line2);
 
 #endif /* LCD_H */
--- lcd/src/lcd.c
+++ lcd/src/lcd.c
@@ -16,6 +16,12 @@ static void lcd_write(select_t select, uint8_t data);
 static void lcd_enable(lcd_t *lcd);
 static void lcd_send(lcd_t *lcd, uint8_t cmd, select_t select);
 
+/* lcd_write writes out given values to the gpio pins.  the data pins must
+ * correspond to the lcd pins as follows:
+ * data1 --> D4
+ * data2 --> D5
+ * data3 --> D6
+ * data4 --> D7 */
 static void
 lcd_write(lcd_t *lcd, select_t select, uint8_t data)
 {
@@ -26,6 +32,9 @@ lcd_write(lcd_t *lcd, select_t select, uint8_t data)
 	HAL_GPIO_WritePin(lcd->gpio_data, lcd->data4, (data >> 3) & 1);
 }
 
+/* lcd_enable activates the enable pin on the lcd, which causes a command or
+ * data to be sent on the falling edge of a pulse.  the enable pin must be held
+ * for 450ns, but is held for 1ms by the delays */
 static void
 lcd_enable(lcd_t *lcd)
 {
@@ -35,15 +44,21 @@ lcd_enable(lcd_t *lcd)
 	HAL_Delay(1);
 }
 
+/* lcd_send is a helper function which sends an eight byte command to the lcd
+ * which uses four bit mode, thus requiring two writes */
 static void
 lcd_send(lcd_t *lcd, select_t select, uint8_t cmd)
 {
 	lcd_write(lcd, select, (cmd & 0xF0) >> 4);
 	lcd_enable(lcd);
-	lcd_write(lcd, selecct, cmd * 0xF);
+	lcd_write(lcd, selecct, cmd & 0xF);
 	lcd_enable(lcd);
 }
 
+/* lcd_reset follows the procedure found on page 46 of the HD44780U data sheet,
+ * which specifies how to reset the lcd and enter four bit mode.  three 0x3
+ * bytes are sent, followed by a 0x2 byte.  then the commands commented below
+ * are run */
 void
 lcd_reset(lcd_t *lcd)
 {
@@ -57,14 +72,20 @@ lcd_reset(lcd_t *lcd)
 	lcd_write(lcd, COMMAND, 0x2);
 	lcd_enable(lcd);
 
-	lcd_send(lcd, COMMAND, 0x28); /* four bit input, two lines, 5x10 font */
+	lcd_send(lcd, COMMAND, 0x28); /* four bit input, two lines, 5x8 font */
 	lcd_send(lcd, COMMAND, 0x08); /* display off */
 	lcd_send(lcd, COMMAND, 0x01); /* clear and return to first line */
-	HAL_Delay(5);
+	HAL_Delay(1);                 /* above command requires 1.37ms delay,
+                                       * only guaranteed 1ms */
 	lcd_send(lcd, COMMAND, 0x06); /* shift right after char input */
-	lcd_send(lcd, COMMAND, 0x0C); /* display on, cursor on */
+	lcd_send(lcd, COMMAND, 0x0C); /* display on, cursor off */
 }
 
+/* lcd_string clears the screen then sends a series of up to 16 characters for
+ * each line.  after writing the first line, a command is run to move the
+ * write address to 0x40, which is 64 characters into the rom in the lcd and
+ * the start of the second line
+ */
 void
 lcd_string(lcd_t *lcd, const char *line1, const char *line2)
 {
